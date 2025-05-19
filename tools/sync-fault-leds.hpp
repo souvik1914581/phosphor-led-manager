d363ce7be48f39393fabc45971517e822133056e
Souvik Roy <souvik.roy10@ibm.com>
2025-05-19 03:36:38 -0500

    Implement sync fault LED option in led-tool(#91)
    This commit implements an option in led-tool to sync asserted and state
    properties of the fault LED of a FRU according to the functional
    property of the FRU.
    
    Test:
    ```
    1. On a rainier 2s2u simics system, check functional status of FRU fan0,
       and see it is functional, check asserted property of fan0_fault LED
       and it is false, check physical LED state, and it is off.
    2. Using busctl, turn physical LED on.
    3. Run "led-tool --syncFaultLed -o /system/chassis/motherboard/fan0"
    4. Physical LED goes off.
    5. Using busctl, set asserted property of fan_fault LED to true.
    6. Physical LED state turns on
    7. Run "led-tool --syncFaultLed -o /system/chassis/motherboard/fan0"
    8. Asserted property of fan0_fault LED becomes false and physical LED
       state turns off.
    9. From ASMI, turn on identity LED for fan0
    10. Physical LED goes to blink state.
    11. Run "led-tool --syncFaultLed -o /system/chassis/motherboard/fan0"
    12. Physical LED stays in blink state.
    13. Using busctl, set functional property of fan0 to false
    14. Asserted property of fan0_fault LED becomes true and physical LED
        turns on.
    15. Using busctl, turn off physical LED.
    16. Run "led-tool --syncFaultLed -o /system/chassis/motherboard/fan0"
    17. Physical LED turns on.
    ```
    
    Signed-off-by: Souvik Roy <souvik.roy10@ibm.com>

--- tools/sync-fault-leds.hpp
+++ tools/sync-fault-leds.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include "utility.hpp"
 
-/** @brief API to sync asserted and state properties of a fault LED of a FRU.
+/**
+ * @brief API to sync asserted and state properties of a fault LED of a FRU.
  *
  * This API syncs asserted and state properties of a fault LED of a FRU
  * according to the functional property of the FRU.
@@ -10,14 +11,186 @@
  *
  * @throw std::runtime_error
  */
-void doSyncFaultLed([[maybe_unused]] const std::string& i_objectPath)
+void doSyncFaultLed(const std::string& i_objectPath)
 {
-    // TODO:
-    // 1. For given object path, get the Functional property from PIM
-    // 2. Construct associated fault LED object path and get the Asserted
-    //    property from LED.Group
-    //    If Asserted property is incorrect, update it.
-    // 3. Construct associated physical LED object path and get the State
-    //    property from LED.Controller.pca955x_* service.
-    //    If State property is incorrect, update it.
+    try
+    {
+        // extract the FRU name from object path
+        const auto l_lastSlashPos{i_objectPath.find_last_of('/')};
+        if (l_lastSlashPos == std::string::npos)
+        {
+            throw std::runtime_error("Invalid object path");
+        }
+
+        const std::string l_fruName = i_objectPath.substr(l_lastSlashPos + 1);
+        std::cout << "FRU name: " << l_fruName << std::endl;
+
+        // lambda to get the Functional property of the FRU
+        auto l_getFunctionalProperty =
+            [&i_objectPath =
+                 std::as_const(i_objectPath)]() -> std::optional<bool> {
+            std::optional<bool> l_result;
+
+            const auto l_functionalProperty = utility::getProperty(
+                "xyz.openbmc_project.Inventory.Manager",
+                "/xyz/openbmc_project/inventory" + i_objectPath,
+                "xyz.openbmc_project.State.Decorator.OperationalStatus",
+                "Functional");
+
+            if (const auto l_functionalPropertyVal =
+                    std::get_if<bool>(&l_functionalProperty))
+            {
+                l_result.emplace(*l_functionalPropertyVal);
+            }
+            return l_result;
+        };
+
+        // lambda to get the Asserted property of the fault LED
+        auto l_getAssertedProperty =
+            [](const std::string& i_fruName) -> std::optional<bool> {
+            std::optional<bool> l_result;
+
+            const auto l_assertedProperty = utility::getProperty(
+                "xyz.openbmc_project.LED.GroupManager",
+                "/xyz/openbmc_project/led/groups/" + i_fruName + "_fault",
+                "xyz.openbmc_project.Led.Group", "Asserted");
+
+            if (const auto l_assertedPropertyValue =
+                    std::get_if<bool>(&l_assertedProperty))
+            {
+                l_result.emplace(*l_assertedPropertyValue);
+            }
+            return l_result;
+        };
+
+        // lambda to get the physical LED state
+        auto l_getPhysicalLedState =
+            [](const std::string& i_fruName) -> std::optional<std::string> {
+            std::optional<std::string> l_result;
+
+            const auto l_assertedProperty = utility::getProperty(
+                "xyz.openbmc_project.LED.Controller.pca955x_" + i_fruName,
+                "/xyz/openbmc_project/led/physical/pca955x_" + i_fruName,
+                "xyz.openbmc_project.Led.Physical", "State");
+
+            if (const auto l_assertedPropertyValue =
+                    std::get_if<std::string>(&l_assertedProperty))
+            {
+                l_result.emplace(*l_assertedPropertyValue);
+            }
+            return l_result;
+        };
+
+        // lambda to set the Asserted property of the fault LED
+        auto l_setAssertedProperty = [](const std::string& i_fruName,
+                                        const bool i_value) -> bool {
+            return utility::setProperty<bool>(
+                "xyz.openbmc_project.LED.GroupManager",
+                "/xyz/openbmc_project/led/groups/" + i_fruName + "_fault",
+                "xyz.openbmc_project.Led.Group", "Asserted", i_value);
+        };
+
+        // lambda to set the Physical property of the LED
+        auto l_setPhysicalLedState = [](const std::string& i_fruName,
+                                        const std::string& i_value) -> bool {
+            return utility::setProperty<std::string>(
+                "xyz.openbmc_project.LED.Controller.pca955x_" + i_fruName,
+                "/xyz/openbmc_project/led/physical/pca955x_" + i_fruName,
+                "xyz.openbmc_project.Led.Physical", "State", i_value);
+        };
+
+        // get the Functional property from PIM
+        const auto l_functionalProperty = l_getFunctionalProperty();
+        if (l_functionalProperty.has_value())
+        {
+            std::cout << "Current Functional value: " << std::boolalpha
+                      << l_functionalProperty.value() << std::endl;
+            // get the Asserted property of the fault LED
+            const auto l_assertedProperty = l_getAssertedProperty(l_fruName);
+            if (l_assertedProperty.has_value())
+            {
+                std::cout << "Current Asserted value: " << std::boolalpha
+                          << l_assertedProperty.value() << std::endl;
+
+                // Asserted is inverse of functional status.
+                if (l_functionalProperty.value() == l_assertedProperty.value())
+                {
+                    if (!l_setAssertedProperty(l_fruName,
+                                               !l_functionalProperty.value()))
+                    {
+                        throw std::runtime_error(
+                            "Failed to update Asserted property");
+                    }
+                    std::cout << "Asserted value updated to: " << std::boolalpha
+                              << !l_functionalProperty.value() << std::endl;
+                }
+
+                // get the physical LED state
+                const auto l_physicalLedState =
+                    l_getPhysicalLedState(l_fruName);
+                if (l_physicalLedState.has_value())
+                {
+                    std::cout << "Current Physical LED state: "
+                              << l_physicalLedState.value() << std::endl;
+
+                    // if LED is currently blinking, it means identify has been
+                    // set, which has higher precedence, so we do nothing.
+                    if (l_physicalLedState.value() !=
+                        "xyz.openbmc_project.Led.Physical.Action.Blink")
+                    {
+                        if (l_functionalProperty.value() &&
+                            l_physicalLedState.value() ==
+                                "xyz.openbmc_project.Led.Physical.Action.On")
+                        {
+                            // turn off physical LED as FRU is functional
+                            if (!l_setPhysicalLedState(
+                                    l_fruName,
+                                    "xyz.openbmc_project.Led.Physical.Action.Off"))
+                            {
+                                throw std::runtime_error(
+                                    "Failed to turn off physical LED");
+                            }
+                            std::cout << "Physical LED turned off" << std::endl;
+                        }
+                        else if (
+                            !l_functionalProperty.value() &&
+                            l_physicalLedState.value() ==
+                                "xyz.openbmc_project.Led.Physical.Action.Off")
+                        {
+                            // turn on physical LED as FRU is not functional
+                            if (!l_setPhysicalLedState(
+                                    l_fruName,
+                                    "xyz.openbmc_project.Led.Physical.Action.On"))
+                            {
+                                throw std::runtime_error(
+                                    "Failed to turn on physical LED");
+                            }
+                            std::cout << "Physical LED turned on" << std::endl;
+                        }
+                    }
+                }
+                else
+                {
+                    throw std::runtime_error(
+                        "Failed to get physical state of LED");
+                }
+            }
+            else
+            {
+                throw std::runtime_error(
+                    "Failed to get Asserted property value.");
+            }
+        }
+        else
+        {
+            throw std::runtime_error(
+                "Failed to get Functional property value.");
+        }
+    }
+    catch (const std::exception& l_ex)
+    {
+        throw std::runtime_error(
+            "Failed to sync fault LED properties for FRU [" + i_objectPath +
+            "]. Error: " + std::string(l_ex.what()));
+    }
 }
